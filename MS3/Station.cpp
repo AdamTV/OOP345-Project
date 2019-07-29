b45f0e6b40723bcf7badc53701aee32390f6ba1c
AdamTV <43098728+AdamTV@users.noreply.github.com>
2019-07-29 17:29:35 -0400

    Update Station.cpp

--- MS3/Station.cpp
+++ MS3/Station.cpp
@@ -22,23 +22,38 @@
 #include "Station.h"
 
 namespace sict {
+
+	// custom constructor to set data members
+	//
 	Station::Station(const std::string& str) : items(ItemSet(str))
 	{
 		name = getName();
 	}
+
+	// method to display items object to any ostream
+	//
 	void Station::display(std::ostream& os) const
 	{
 		items.display(os, true);
 	}
+
+	// method to fill the last order in the customer order queue
+	//
 	void Station::fill(std::ostream& os)
 	{
 		if (!custOrders.empty())
 			custOrders.back().fillItem(items, os);
 	}
+
+	// forwarding query to get reference to name of items object
+	//
 	const std::string& Station::getName() const
 	{
 		return items.getName();
 	}
+
+	// query that returns the release state of the current object
+	//
 	bool Station::hasAnOrderToRelease() const
 	{
 		bool has = false;
@@ -50,16 +65,25 @@ namespace sict {
 		}
 		return has;
 	}
+
+	// decrements number of items in items object
+	//
 	Station& Station::operator--()
 	{
 		items.operator--();
 		return *this;
 	}
+
+	// moves rvalue reference to the back of the station's queue
+	//
 	Station& Station::operator+=(CustomerOrder&& order)
 	{
 		custOrders.push(std::move(order));
 		return *this;
 	}
+
+	// removes order at the front of queue moves it to calling function
+	//
 	bool Station::pop(CustomerOrder& ready)
 	{
 		bool filled = false;
@@ -70,6 +94,9 @@ namespace sict {
 		}
 		return filled;
 	}
+
+	// reports the state of the items object
+	//
 	void Station::validate(std::ostream& os) const
 	{
 		os << " getName(): " << items.getName() << std::endl;
